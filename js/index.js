92c9bbe5691cd7acd8e65d5a233703efde2a6300
Emilio Lubo <emilubo@msn.com>
2023-03-01 00:27:02 -0300

    functional fix

--- js/index.js
+++ js/index.js
@@ -1,46 +1,61 @@
-let user = JSON.parse(localStorage.getItem("User"))
-if(!user){
-    try{
-        fetch('https://randomuser.me/api/')
-        .then(res => res.json())
-        .then(data => {
-            console.log(data.results)
-            const {name, phone, picture, dob, email, location} = data.results[0]
-            const date = new Date(dob.date).toLocaleDateString()
-            const user = {
-                data: {
-                    fName: name.first,
-                    lName: name.last,
-                    birth: date,
-                    location: location.country,
-                },
-                others: {
-                    email: email,
-                    phone: phone,
-                    photo: picture.large,
+let user
+
+getData()
+
+if(document.title === "Mi CV"){
+    let button = document.querySelector('#button-change')
+    button.addEventListener('click', e => {
+        localStorage.clear()
+        getData()
+    })
+}
+
+function getData(){
+    user = JSON.parse(localStorage.getItem("User"))
+    if(!user){
+        try{
+            fetch('https://randomuser.me/api/')
+            .then(res => res.json())
+            .then(data => {
+                const {name, phone, picture, dob, email, location} = data.results[0]
+                const date = new Date(dob.date).toLocaleDateString()
+                const user = {
+                    data: {
+                        fName: name.first,
+                        lName: name.last,
+                        birth: date,
+                        location: location.country,
+                    },
+                    others: {
+                        email: email,
+                        phone: phone,
+                        photo: picture.large,
+                    }
                 }
-            }
-            setData(user)
-            localStorage.setItem("User", JSON.stringify(user))
-        })
-    } catch(err) {
-        console.log(err.message)
+                setData(user)
+                localStorage.setItem("User", JSON.stringify(user))
+            })
+        } catch(err) {
+            console.log(err.message)
+        }
+    } else {
+        setData(user)
     }
-} else {
-    setData(user)
 }
 
 function setData(u){
     let photo = document.querySelector("#photo")
-    let info = document.querySelectorAll(".info")
     let mail = document.querySelector("#mail")
     let tel = document.querySelector("#tel")
-    let i = 0
     let {data, others} = u
     photo.src = others.photo
-    for(let dato in data){
-            info[i].innerHTML = data[dato]
-         i++
+    if(document.title === "Mi CV"){
+        let info = document.querySelectorAll(".info")
+        let i = 0
+        for(let dato in data){
+                info[i].innerHTML = data[dato]
+             i++
+        }
     }
     mail.href = "mailto:" + others.email
     tel.href = others.phone
